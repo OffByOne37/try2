7d64c106cf0e9504f598057fe158c2a55783d683
OffByOne37 <129181101+OffByOne37@users.noreply.github.com>
2023-03-28 14:56:58 +0200

    Aktualisiere main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,16 @@
 
 namespace screenMagic {
-/**
- * asdfasdfasdf
- */
-export function plotAt(index: number): void {
-    index |= 0;
-    const y = Math.floor(index / 5);
-    const x = Math.floor(index % 5);
-    led.plot(x, y);
-}
+    /**
+     * asdfasdfasdf
+     */
+    //% blockId=screenmagicplotat
+    //% block ="plot at index"
+    //% index.min=0 index.max=25
+    export function plotAt(index: number): void {
+        index |= 0;
+        const y = Math.floor(index / 5);
+        const x = Math.floor(index % 5);
+        led.plot(x, y);
+    }
 			
 }
