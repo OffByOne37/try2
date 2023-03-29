ddc6cbe93fdc30a699530c828fb90bb770b4118f
OffByOne37 <129181101+OffByOne37@users.noreply.github.com>
2023-03-29 10:50:05 +0200

    Aktualisiere main.ts

--- main.ts
+++ main.ts
@@ -4,13 +4,35 @@ namespace screenMagic {
      * asdfasdfasdf
      */
     //% blockId=screenmagicplotat
-    //% block ="plot at index"
+    //% block ="plot at $index"
     //% index.min=0 index.max=25
     export function plotAt(index: number): void {
         index |= 0;
         const y = Math.floor(index / 5);
         const x = Math.floor(index % 5);
         led.plot(x, y);
+    }
+    /**
+     * asdfasdfasdfaefg
+     */
+    //% blockId=screenmagicplotat
+    //% block ="plot row at $index"
+    //% index.min=0 index.max=4
+    export function drawRectangle(row: number): void{
+        let startPoint = (row * 5)+ row;
+        let limit = Math.abs(row -5);
+        for(let i = startPoint; i < (startPoint+limit); i++){
+            plotAt(i);
+        }
+        for(let i = (startPoint + limit); i <= 25;i= i+5){
+            plotAt(i);
+        }
+        for(let i = 24; i > (25-limit); i--){
+            plotAt(i);
+        }
+        for (let i = 25-limit; i > row; i= i-5) {
+            plotAt(i);
+        }
     }
 			
 }
