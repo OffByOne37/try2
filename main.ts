eedd9bfca960a3687b8a3fed92ccf25d5106cec7
OffByOne37 <129181101+OffByOne37@users.noreply.github.com>
2023-03-29 10:53:50 +0200

    Aktualisiere main.ts

--- main.ts
+++ main.ts
@@ -23,15 +23,19 @@ namespace screenMagic {
         let limit = Math.abs(row -5);
         for(let i = startPoint; i < (startPoint+limit); i++){
             plotAt(i);
+            basic.pause(100);
         }
         for(let i = (startPoint + limit); i <= 25;i= i+5){
             plotAt(i);
+            basic.pause(100);
         }
         for(let i = 24; i > (25-limit); i--){
             plotAt(i);
+            basic.pause(100);
         }
         for (let i = 25-limit; i > row; i= i-5) {
             plotAt(i);
+            basic.pause(100);
         }
     }
 			
